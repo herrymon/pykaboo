11f3aa026303f8dc12049bcb1060e8a11405abcd
Erick <erick@mercury.(none)>
2010-01-23 02:41:21 +0800

    transfer EXT constant from config.py to pyka.py
    fix error on reading wsgi.input, wsgi.input must be cached on __init__ of Request
    
    add POST property in Controller, more convenient to call self.POST than self.request.post

--- pyka.py
+++ pyka.py
@@ -6,6 +6,7 @@ codepoint tutorial @see http://webpython.codepoint.net/wsgi_tutorial
 Code concepts, ideas borrowed from:
 web.py @see http://webpy.org/
 bottle.py @see http://github.com/defnull/bottle.
+webob @see http://bitbucket.org/ianb/webob/ 
 werkzeug @see http://werkzeug.pocoo.org/
 django @see http://www.djangoproject.com
 ''' 
@@ -18,6 +19,9 @@ import os, sys
 sys.path.append(os.path.dirname(__file__))   #@see http://code.google.com/p/modwsgi/wiki/IntegrationWithDjango
 import config
 from datetime import datetime
+
+'''constants'''
+EXT = '.py'
 
 '''errors and logger'''
 class HTTPRequestException(Exception): pass
@@ -38,7 +42,7 @@ else:
 #utility functions
 def dictify(input_text):
     '''convert wsgi.input to {}, uses built-in urlparse module
-Eg name=Erick&time=11pm to {'name': ['Erick'], 'time': ['11pm']}
+Eg name=Erick&pets=rat&pets=cats to {'name': 'Erick', 'pets': ['rats', 'cats']}
 '''
     log('xutils.dictify: %d %s' %(len(input_text), input_text))
     from urlparse import parse_qs
@@ -46,7 +50,7 @@ Eg name=Erick&time=11pm to {'name': ['Erick'], 'time': ['11pm']}
     dict_input = parse_qs(input_text)
     # use cgi.escape built-in to escape "&<>" from GET and POST, or other inputs
     for key in dict_input:
-       dict_input[key] = [escape(val) for val in dict_input[key]]
+        dict_input[key] = [escape(val) for val in dict_input[key]]
     return dict_input
 
 def _404():
@@ -87,9 +91,10 @@ class Response(object):
         if self.request.method == 'HEAD':
             return ''
         else:
-            if booger:
+            if booger and config.DEBUG:
                 from cgi import escape
-                self.add('<pre>')
+                self.add('<pre style="background:#fdd;border:1px solid #ecc;margin:0;padding:1em">')
+                self.add('<h3 style="margin:0;padding:0.2em;line-height:1.5em;background:#800;color:#fff">config.DEBUG is on, turn off debugging to hide this box</h3>')
                 for b in booger:
                     self.add([escape(b)])
                 self.add('</pre>')
@@ -107,34 +112,41 @@ class Request(object):
     '''container of WSGI environ object'''
     def __init__(self, _env):
         self.environ = _env
-        self.content_length = _env.get('CONTENT_LENGTH',0)
-        self.path_info = _env.get('PATH_INFO','')
+        self.content_length = self.environ.get('CONTENT_LENGTH',0)
+        self.path_info = self.environ.get('PATH_INFO','')
         self.path = self.path_info.split('/')
+        self._post_cache()
         from Cookie import SimpleCookie
-        self.cookie = SimpleCookie(_env.get('HTTP_COOKIE',''))
+        self.cookie = SimpleCookie(self.environ.get('HTTP_COOKIE',''))
+
+    def _post_cache(self):
+        '''on __init__ , wsgi.input must be read, otherwise I'm getting empty wsgi.input'''
+        input = self.environ.get('wsgi.input','')
+        if input:
+            self.post_cache = input.read()
+        else:
+            self.post_cache = ''
 
     @property
     def method(self):
         _supported_methods = ('GET', 'POST')
         _method = self.environ.get('REQUEST_METHOD', None)
         if _method and _method in _supported_methods:
-            return _method.lower()
+            return _method.lower() # why lower? need to map this later to Controller method Eg def get(self)...
         else:
             raise HTTPRequestException('method is not supported. Only GET and POST is supported at the moment...')
 
     @property
     def post(self):
-        _post = self.environ.get('wsgi.input','')
-        if _post:
-            return dictify(_post.read(self.content_length))
-        else:
-            return {}
+        return dictify(self.post_cache)
 
     @property
     def query_string(self):
         return dictify(self.environ.get('QUERY_STRING',''))
         
-    def __str__(self):
+    @property
+    def debug(self):
+        ''' string representation of environ params, for debugging purposes :)'''
         return \
 '''method: %s
 post: %s
@@ -190,6 +202,10 @@ class Controller(object):
     def __init__(self, _request):
         self.request = _request
 
+    @property
+    def POST(self):
+        return self.request.post
+
 
 class Header(object):
     '''status object and response headers packed to use as arg to start_response()'''
@@ -215,25 +231,26 @@ class App(object):
     def __call__(self, environ, start_response):
         from time import time
         start_time = time()
-        req = Request(environ)
-        resp = Response(req)
 
         try:
+            req = Request(environ)
+            resp = Response(req)
             #check if path is in ROUTES get controller mapped to route
             response_echo = ''
-            booger = [str(req)]
+            booger = ['\n****Request object****\n']
+            booger.append(req.debug)
             for route in config.ROUTES:
                 url = route[0]
                 module, klass = route[1].split('.', 1)
-                booger.append('\nurl:%s\ncontroller script:%s\nklass:%s'%(url, module, klass))
                 if url == req.path_info:
-                    if self._controller_exists(module + config.EXT):
-                        # only supporting POST GET HEAD for now, have to read up about PUT DELETE HEAD OPTIONS and other methods
+                    if self._controller_exists(module + EXT):
                         controller = self._get_controller(module, klass, req)
                         func = getattr(controller, req.method)
-                        resp.add(str(req.cookie.keys()))
                         resp.add([func()])
-                        booger.append('\nstr(func)' + str(func))                        
+                        booger.append('\nurl:{0}'.format(url))
+                        booger.append('\ncookie_keys:{0}'.format(','.join(req.cookie.keys())) )
+                        booger.append('\n\n****Controller object****')
+                        booger.append('\nfile.controller.method:{f}.{c}.{m}'.format(f=module, c=klass, m=func.__name__) )                        
                         resp.cookie_header(req.cookie, req.cookie.keys())
 
                         # break from route loop
@@ -247,6 +264,7 @@ class App(object):
             response_echo = resp(booger)
         except:
             # @see http://lucumr.pocoo.org/2007/5/21/getting-started-with-wsgi 
+            booger = []
             from sys import exc_info
             from traceback import format_tb
             e_type, e_value, tb = exc_info() 
@@ -261,7 +279,7 @@ class App(object):
             response_echo = '<br/>'.join(traceback) + ''.join(booger)
         finally:
             log('pykaboo rendered in approximately: %f' %(time()-start_time,))
-            start_response(*resp.header.pack)
+            start_response(*resp.header.pack[:2])
             yield response_echo
                 
     def _controller_exists(self, file):
