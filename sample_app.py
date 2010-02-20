177e710e330bb3c25c384a95d1c02189f10cc2c8
Erick <erick@mercury.(none)>
2010-02-21 07:58:57 +0800

    change @bind handlers to @post and @get

--- sample_app.py
+++ sample_app.py
@@ -1,25 +1,36 @@
 # sample pykaboo app
 import os.path
-from pyka import application, bind, serve
+from pyka import application, serve, post, get, bind
 from pyka import mako_render
 from pyka import PYKA_PATH
 
 template_path = os.path.join(PYKA_PATH, 'html')
 
-@bind('/')
+@get('/')
 def index():
     return 'index of app' + index.request.base_url
 
-@bind('/hello')
-def hello():
-    return 'hello' + hello.request.path[0]
-
-@bind('/greet/[a-z]*')
+@get('/greet/[a-z]*')
 def greet():
     return 'hello, ' + greet.request.path[1]
 
-@bind('/mako')
+@get('/mako')
 def templated():
     return mako_render('mako_sample.html', template_paths=[template_path], body="test")
 
-serve(application)
+@get('/sayform')
+def sayform():
+    return """
+    <form method="post" action="/say">
+    <input type=text name="message"/>
+    <input type=text name="message"/>
+    <input type=submit value="go"/>
+    </form>"""
+
+@post('/say')
+def say():
+    msg = say.request.post.getvalue('message')
+    return msg
+
+if __name__ == '__main__':
+    serve(application)
